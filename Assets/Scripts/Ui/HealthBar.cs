8d843d66e7e8dc0cb1a012c1e9b38793074e1baa
Holo Laptop <holo@frozent.pl>
2024-01-26 23:18:21 +0100

    Added health bar scaling with percent

--- Assets/Scripts/Ui/HealthBar.cs
+++ Assets/Scripts/Ui/HealthBar.cs
@@ -1,8 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
 	[SerializeField]
-	private Image _healthLabel;
+	private Image _healthImg;
+
+	public Image HealthImg => _healthImg;
+
+	private float _width;
+
+	private float max = 1;
+
+	public void Awake()
+	{
+		_width = GetComponent<RectTransform>().rect.width;
+	}
+
+	public void SetByPercent(float percent)
+	{
+		float reversedPercent = 1 - percent;
+		HealthImg.rectTransform.offsetMax = new Vector2(-_width * reversedPercent, HealthImg.rectTransform.offsetMax.y);
+	}
 }
