70fa4797026f6fcb0bbe3a04222e7cde36d021b4
Holo Laptop <holo@frozent.pl>
2024-01-27 02:49:38 +0100

    Timer and TMP added

---
+++ Assets/Scripts/Systems/TimerSystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class TimerSystem : GameSystem
+{
+	[SerializeField]
+	private TMP_Text _label;
+
+	private float _time;
+
+	public void ResetTimer()
+	{
+		_time = 0;
+	}
+
+	private string DoublePad(float num)
+	{
+		if (num < 10)
+		{
+			return "0" + (int)num;
+		}
+
+		return ((int)num).ToString();
+	}
+
+	public string GetReadableTime()
+	{
+		return DoublePad(_time/60) + ":" + DoublePad(_time%60) + "." + DoublePad((_time%1)*100);
+	}
+	
+	public void Update()
+	{
+		_time += Time.deltaTime*10;
+		_label.SetText(GetReadableTime());
+	}
+}
