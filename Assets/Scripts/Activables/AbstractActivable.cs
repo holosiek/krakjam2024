59ae4c675d0b6a914a3e492855141fd8223a95f4
MaxPortablePC <dziadon.kakuzu@gmail.com>
2024-01-27 15:47:36 +0100

    activable state checkers

--- Assets/Scripts/Activables/AbstractActivable.cs
+++ Assets/Scripts/Activables/AbstractActivable.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public abstract class AbstractActivable : MonoBehaviour
 {
+    public virtual bool IsActive { get; protected set; } = false;
+
     public abstract void Activate();
     public abstract void Deactivate();
 }
--- Assets/Scripts/Activables/ActivableGameObject.cs
+++ Assets/Scripts/Activables/ActivableGameObject.cs
@@ -3,10 +3,12 @@
     public override void Activate()
     {
         gameObject.SetActive(true);
+        IsActive = true;
     }
 
     public override void Deactivate()
     {
         gameObject.SetActive(false);
+        IsActive = false;
     }
 }
