615594e2992feefea3df529f44ffaf1feb5b0874
Holo Laptop <holo@frozent.pl>
2024-01-27 07:50:40 +0100

    Added datasystem

---
+++ Assets/Scripts/Systems/DataSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public class DataSystem : GameSystem
+{
+	public List<float> BestTimes = new List<float>();
+
+	public void AddNewBestTime(float time)
+	{
+		for (int index = 0; index < BestTimes.Count; index++)
+		{
+			float bestTime = BestTimes[index];
+			
+			if (time < bestTime)
+			{
+				BestTimes.Insert(index, time);
+				break;
+			}
+		}
+
+		if (BestTimes.Count > 10)
+		{
+			BestTimes.RemoveAt(10);
+		}
+	}
+}
