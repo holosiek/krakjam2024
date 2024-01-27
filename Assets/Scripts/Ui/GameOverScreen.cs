31dbf112a3cda9b0f756cfb34308b212f30f61e8
Holo Laptop <holo@frozent.pl>
2024-01-27 13:52:07 +0100

    Stopping timer and added timer to gameover screen

--- Assets/Scripts/Ui/GameOverScreen.cs
+++ Assets/Scripts/Ui/GameOverScreen.cs
@@ -1,12 +1,20 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class GameOverScreen : MonoBehaviour
 {
 	[SerializeField]
 	private CanvasGroup _gameOverScreen;
 
+	[SerializeField] 
+	private TMP_Text _timerLabel;
+
 	public void ShowGameOverScreen()
 	{
+		var timerSystem = GameInstance.Instance.Get<TimerSystem>();
+		float time = timerSystem.GetTime();
+		_timerLabel.SetText(timerSystem.GetReadableTime(time));
+		timerSystem.StopTimer();
 		_gameOverScreen.alpha = 1;
 	}
 }
